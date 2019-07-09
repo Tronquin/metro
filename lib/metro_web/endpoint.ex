4b72f6fa843322a3ee45fae216a8d6399c3948fe
Francisco Bejarano <fbejarano@jacidi.com>
2019-07-09 15:03:30 -0400

    fonts added, scss partials finished (so far), navbar finished

--- lib/metro_web/endpoint.ex
+++ lib/metro_web/endpoint.ex
@@ -1,51 +1,56 @@
 defmodule MetroWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :metro
 
-#  socket "/live", Phoenix.LiveView.Socket
+  #  socket "/live", Phoenix.LiveView.Socket
 
-  socket "/socket",
-         MetroWeb.UserSocket,
-         websocket: true,
-           # or list of options
-         longpoll: false
+  socket(
+    "/socket",
+    MetroWeb.UserSocket,
+    websocket: true,
+    # or list of options
+    longpoll: false
+  )
 
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
-  plug Plug.Static,
-       at: "/",
-       from: :metro,
-       gzip: false,
-       only: ~w(css fonts images js favicon.ico robots.txt)
+  plug(Plug.Static,
+    at: "/",
+    from: :metro,
+    gzip: true,
+    only: ~w(css fonts images js favicon.ico robots.txt)
+  )
 
-         # Code reloading can be explicitly enabled under the
-         # :code_reloader configuration of your endpoint.
+  # Code reloading can be explicitly enabled under the
+  # :code_reloader configuration of your endpoint.
   if code_reloading? do
-    socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
-    plug Phoenix.LiveReloader
-    plug Phoenix.CodeReloader
+    socket("/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket)
+    plug(Phoenix.LiveReloader)
+    plug(Phoenix.CodeReloader)
   end
 
-  plug Plug.Logger
+  plug(Plug.Logger)
 
-  plug Plug.Parsers,
-       parsers: [:urlencoded, :multipart, :json],
-       pass: ["*/*"],
-       json_decoder: Jason
+  plug(Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Jason
+  )
 
-  plug Plug.MethodOverride
-  plug Plug.Head
+  plug(Plug.MethodOverride)
+  plug(Plug.Head)
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
   # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-       store: :cookie,
-       key: "_metro_key",
-       signing_salt: "NfWbiWHR"
+  plug(Plug.Session,
+    store: :cookie,
+    key: "_metro_key",
+    signing_salt: "NfWbiWHR"
+  )
 
-  plug MetroWeb.Router
+  plug(MetroWeb.Router)
 
   @doc """
   Callback invoked for dynamically configuring the endpoint.
